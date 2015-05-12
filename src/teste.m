a186ecfc8f4a9087067a1068fdbc81e5512091da
Lucas Romão <lucas.romao.silva@gmail.com>
2015-05-11 21:57:17 -0300

    Captura de entrada do problema pelo usuario, preciso adicionar no relatorio como e o formato de entrada

--- src/teste.m
+++ src/teste.m
@@ -1,6 +1,27 @@
-#!/usr/bin/octave -qf
-1;
-a=1;
-b=2;
-c=1+b;
-disp(c);
+#! /bin/octave -qf
+m = scanf("%d", 1);
+n = scanf("%d", 1);
+A = rand(m, n);
+b = xb = zero(m, 1);
+c = zero(1, n);
+global base = zero(m, 1);
+global is_base = zero(n, 1);
+for j = 1:n
+    c(j) = scanf
+endfor
+for i = 1:m
+    for j = 1:n
+        A(i, j) = scanf("%f", 1);
+    endfor
+endfor
+for i = 1:m
+endfor
+for i = 1:m
+    b(i) = scanf("%f", 1);
+endfor
+for i = 1:m
+    base(i) = scanf("%d", 1);
+    xb(i) = scanf("%f", 1);
+    is_base(base(i)) = 1;
+endfor
+global invB = inv(A(:, base)); 
