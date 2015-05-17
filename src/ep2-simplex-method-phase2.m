5b643e4985030274fb326e03d23d3d412c758bde
Lucas Romão <lucas.romao.silva@gmail.com>
2015-05-17 16:14:17 -0300

    Mudancas no ep e adicionados traces de alguns casos

--- src/ep2-simplex-method-phase2.m
+++ src/ep2-simplex-method-phase2.m
@@ -18,7 +18,7 @@ function [theta, imin] = calculate_theta(u, m, x, base)
   theta = Inf;
   imin = 0;
   for i=1:m
-    if(u(i) > 0)
+    if(u(i) > -10^-10)
       if(x(i)/u(i) < theta)
         theta = x(i)/u(i);
         imin = i;
@@ -54,18 +54,21 @@ function [ind v] = simplex(A, b, c, m, n, x)
     is_base(base) = 1;
 
     indx_j = 0;
+    achou = 0;
+    c_bar = zeros(n, 1);
     printf("\nCustos reduzidos\n")
-    for indx_j = 1:n
-      if(is_base(indx_j) == 0)
-        c_bar = calculate_c_bar(c , indx_j, p_line, A);
-        printf("%d : %f\n",indx_j, c_bar);
-        if (c_bar < 0)
-          break;
+    for j = 1:n
+      if(is_base(j) == 0)
+        c_bar(j) = calculate_c_bar(c , j, p_line, A);
+        printf("%d : %f\n",j, c_bar(j));
+        if (c_bar(j) < -10^-10 && !achou)
+            indx_j = j;
+            achou = 1;
         endif
       endif
     endfor
-
-    if(c_bar >= 0)
+    
+    if (indx_j == 0)
       ind = 0;
       v = zeros(n, 1);
       v(base) = xb;
@@ -96,19 +99,36 @@ function [ind v] = simplex(A, b, c, m, n, x)
     endif
 
     printf("\nTheta*\n%f\n",theta);
-    printf("\nSai da base: %d\n",imin);
+    printf("\nSai da base: %d\n",old_base_imin);
     count = count +1;
   endwhile
 
 endfunction
 
-m = 2; 
-n = 4;
-A = [2,1,1,0;1,2,0,1];
-b = [4;3];
-base = [1,4];
-c = [-1, -1, 0, 0];
-
+#m = 2; 
+#n = 4;
+#A = [2,1,1,0;1,2,0,1];
+#b = [4;3];
+#base = [1,4];
+#c = [-1, -1, 0, 0];
+#m = 3;
+#n = 6;
+#A = [ 1, 2, 2, 1, 0, 0; 
+#        2, 1, 2, 0, 1, 0;
+#        2, 2, 1, 0, 0, 1;];
+#b = [20; 20; 20];
+#base = [4, 5, 6];
+#c = [-10, -12, -12, 0, 0, 0];
+A = [4.73, -4.73,  10.3,  -10.3, -1,  0, 0, 0, 0;
+    -0.83,  0.83,  1.05,  -1.05,  0, -1, 0, 0, 0;
+    -1.56,  1.56, -0.02,   0.02,  0,  0, 1, 0, 0;
+    -2.11,  2.11, 10.22, -10.22,  0,  0, 0, 1, 0;
+     8.32, -8.32, 12.38, -12.38,  0,  0, 0, 0, 1];
+b = [4.88; -3.61; 1.78; 77.52; 120.26];
+c = [1.5; -1.5; -0.25; 0.25; 0; 0; 0; 0; 0]';
+n = 9;
+m = 5;
+base = [1, 4, 7, 8, 9];
 x = zeros(n , 1);
 x(base) = A(:,base) \ b;
 [ind v] = simplex(A, b, c, m, n, x);
